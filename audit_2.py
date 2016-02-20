2e7cdb36e227b42e5a3e296f432942192adf7657
Meron Goitom <meron.g@live.se>
2016-02-20 17:29:21 +0100

    cleant audit_2

--- audit_2.py
+++ audit_2.py
@@ -28,24 +28,15 @@ OSMFILE = "data/gothenburg_sweden.osm"
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
 
-expected = ["", "Savedalen", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
-            "Trail", "Parkway", "Commons"]
 
 
 
-#expected = ["Olofstorp", "Sävedalen", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
-#            "Trail", "Parkway", "Commons"]
 
 # UPDATE THIS VARIABLE
 mapping = {}
 
 
-def audit_houseNr(street_types, street_name):
-    m = street_type_re.search(street_name)
-    if m:
-        street_type = m.group()
-        if street_type not in expected:
-            street_types[street_type].add(street_name)
+
 
 
 def is_street_name(elem):
