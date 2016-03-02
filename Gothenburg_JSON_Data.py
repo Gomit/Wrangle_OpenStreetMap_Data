63f44041dd9f0750549157465cb0e4dc3c3f583f
Meron Goitom <meron.g@live.se>
2016-03-02 16:09:13 +0100

    Added data-file

--- Gothenburg_JSON_Data.py
+++ Gothenburg_JSON_Data.py
@@ -225,7 +225,7 @@ def test():
     # NOTE: if you are running this code on your computer, with a larger dataset, 
     # call the process_map procedure with pretty=False. The pretty=True option adds 
     # additional spaces to the output, making it significantly larger.
-    data = process_map('data/example.osm', True)
+    data = process_map('data/Udacity_Example_Data', True)
     #pprint.pprint(data)
     
     correct_first_elem = {
